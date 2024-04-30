83e6a2233b0750010ee8f60f3549ef536f3e06dd
TBERT31 <berteau.thomas@hotmail.fr>
2024-04-30 09:51:19 +0200

    Update Home template

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
+import { Olympic } from 'src/app/core/models/Olympic.model';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -8,11 +9,19 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  public olympics$: Observable<Olympic[] | null> = of(null);
 
-  constructor(private olympicService: OlympicService) {}
+
+  public numberOfJo: number = 0;
+  public numberOfCountries: number = 0;
+
+  constructor(
+    private olympicService: OlympicService
+  ) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    this.olympicService.loadInitialData();
+    console.log(this.olympics$);
   }
 }
