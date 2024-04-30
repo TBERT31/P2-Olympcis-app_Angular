46aeaf8d7893fa3de8d3c9557e0a6d1c2d42a3ab
TBERT31 <berteau.thomas@hotmail.fr>
2024-04-30 10:09:46 +0200

    We catch dynamically data of Observable for Boxes in Home component !

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -11,7 +11,6 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 export class HomeComponent implements OnInit {
   public olympics$: Observable<Olympic[] | null> = of(null);
 
-
   public numberOfJo: number = 0;
   public numberOfCountries: number = 0;
 
@@ -20,8 +19,30 @@ export class HomeComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
     this.olympicService.loadInitialData();
+    this.olympics$ = this.olympicService.getOlympics();
+    
     console.log(this.olympics$);
+  
+    this.olympics$.subscribe((data) => {
+      if(data){
+        /* Cas où on  supporse ici qu'il n'y a pas d'erreurs dans les données, 
+        c'est à dire, pas deux fois le même pays dans le tableau. */
+        //this.numberOfCountries = data.length; 
+
+        /* Cas où je ne fait pas confiance dans les données qui me sont transmises dans le json, 
+        et qu'il y a possiblement des doublons de pays*/
+        const contries = new Set(data.map(olympic => olympic.country));
+        this.numberOfCountries = contries.size;
+
+        /* Ici je vais scrapper l'année de participation de chaque pays, 
+        je stock cela dans un set pour éviter les doublons car il y en aura forcement*/
+        const years = new Set(data.flatMap(olympic => olympic.participations.map(participation => participation.year)));
+
+        // Mise à jour du nombre d'années uniques de JO
+        this.numberOfJo = years.size;
+      }
+    })
+  
   }
 }
