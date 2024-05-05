ec36b004790d51320f103fa0098b0ff8e037c2cb
TBERT31 <berteau.thomas@hotmail.fr>
2024-05-05 10:00:34 +0200

    Add others comments for presentation

--- src/app/core/models/Olympic.model.ts
+++ src/app/core/models/Olympic.model.ts
@@ -5,3 +5,36 @@ export class Olympic{
     country!: string;
     participations!: Participation[];
 }
+
+/***
+    Example of Olympic, get from ./assets/mock/olympic.json
+
+    {
+        "id": 1,
+        "country": "Italy",
+        "participations": [
+            {
+                "id": 1,
+                "year": 2012,
+                "city": "Londres",
+                "medalsCount": 28,
+                "athleteCount": 372
+            },
+            {
+                "id": 2,
+                "year": 2016,
+                "city": "Rio de Janeiro",
+                "medalsCount": 28,
+                "athleteCount": 375
+            },
+            {
+                "id": 3,
+                "year": 2020,
+                "city": "Tokyo",
+                "medalsCount": 40,
+                "athleteCount": 381
+            }
+        ]
+    }
+
+***/
--- src/app/core/models/Participation.model.ts
+++ src/app/core/models/Participation.model.ts
@@ -5,3 +5,16 @@ export class Participation {
     medalsCount!: number;
     athleteCount!: number;
 }
+
+/***
+    Example of Participation, get from ./assets/mock/olympic.json
+
+    {
+        "id": 1,
+        "year": 2012,
+        "city": "Londres",
+        "medalsCount": 28,
+        "athleteCount": 372
+    }
+
+***/
