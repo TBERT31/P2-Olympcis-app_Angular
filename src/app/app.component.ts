42ecd12fd2195c3302212dfa31d383aa1502125c
TBERT31 <berteau.thomas@hotmail.fr>
2024-05-09 08:02:05 +0200

    Add ngOnDestroy in app.component.ts in case of

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   
   constructor(
     private olympicService: OlympicService
   ) {}
 
+  private subscription: Subscription = new Subscription();
+
   ngOnInit(): void {
-    this.olympicService.loadInitialData().pipe(take(1)).subscribe();
+    this.subscription = this.olympicService.loadInitialData()
+      .pipe(take(1))
+      .subscribe();
   }
-  /*
-     L'opérateur take(1) garantit que l'observable complète 
-     après avoir émis une seule valeur, ce qui implique qu'il n'est pas nécessaire de se désabonner à l'observable dans ngOnDestroy,
-     car le flux de données s'arrêtera de lui-même après cette unique émission. 
-  */
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
